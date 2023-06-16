0b032cce7935c08cb8aaf2bdffcc7bcb26058525
suzan <suzanalkhdr217@gmail.com>
2023-06-16 14:36:06 +0300

    update script.js file

--- script.js
+++ script.js
@@ -4,31 +4,84 @@ var huawei = document.getElementById("huawei");
 var xpera = document.getElementById("xperia");
 var samsung = document.getElementById("samsung");
 var model = document.getElementById("model");
-var link1 = document.getElementById("link1");
-var link2 = document.getElementById("link2");
-var link3 = document.getElementById("link3");
-var link4 = document.getElementById("link4");
+var Iph = document.getElementById("Iph");
+var Hua = document.getElementById("Hua");
+var Xp = document.getElementById("Xp");
+var Sam = document.getElementById("Sam");
+var iphoneInfo = document.getElementById("IphoneInfo");
+var huaweiInfo = document.getElementById("HuaweiInfo");
+var xperiaInfo = document.getElementById("XperiaInfo");
+var samsungInfo = document.getElementById("SamsungInfo");
+var line = document.getElementById("line");
+var h2 = document.getElementById("h2");
+var h22 = document.getElementById("h22");
 
 iphone.onclick = function () {
     header.style.backgroundImage = "url(imgs/iphone1.jpg)";
+    header.style.backgroundSize = "contain";
     model.innerHTML = "Iphone";
+    Iph.style.color = "white";
+    Hua.style.color = "white";
+    Xp.style.color = "white";
+    Sam.style.color = "white";
+    iphoneInfo.style.display = "block";
+    huaweiInfo.style.display = "none";
+    xperiaInfo.style.display = "none";
+    samsungInfo.style.display = "none";
+    line.style.backgroundColor = "white";
+    model.style.color = "white";
 }
 
 huawei.onclick = function () {
-    header.style.backgroundImage = "url(imgs/huawei.jpg)";
+    header.style.backgroundImage = "url(imgs/huawei11.jpg)";
+    header.style.backgroundSize = "cover";
     model.innerHTML = "Huawei";
+    Iph.style.color = "white";
+    Hua.style.color = "white";
+    Xp.style.color = "white";
+    Sam.style.color = "white";
+    huaweiInfo.style.display = "block";
+    iphoneInfo.style.display = "none";
+    xperiaInfo.style.display = "none";
+    samsungInfo.style.display = "none";
+    model.style.color = "white";
+    line.style.backgroundColor = "white";
+
+
 }
 
 xperia.onclick = function () {
     header.style.backgroundImage = "url(imgs/Xperia.jpg)";
+    header.style.backgroundSize = "contain";
     model.innerHTML = "Xperia";
+    Iph.style.color = "white";
+    Hua.style.color = "white";
+    Xp.style.color = "white";
+    Sam.style.color = "white";
+    xperiaInfo.style.display = "block";
+    huaweiInfo.style.display = "none";
+    iphoneInfo.style.display = "none";
+    samsungInfo.style.display = "none";
+    model.style.color = "black";
+    line.style.backgroundColor = "black";
+    xperiaInfo.style.color = "black";
+    h2.style.color = "black";
 }
 
 samsung.onclick = function () {
     header.style.backgroundImage = "url(imgs/samsung.jpg)";
+    header.style.backgroundSize = "cover";
     model.innerHTML = "Samsung";
-    link1.style.fontStyle = black;
-    link2.style.fontStyle = black;
-    link3.style.fontStyle = black;
-    link4.style.fontStyle = black;
+    Iph.style.color = "black";
+    Hua.style.color = "black";
+    Xp.style.color = "black";
+    Sam.style.color = "black";
+    samsungInfo.style.display = "block";
+    huaweiInfo.style.display = "none";
+    xperiaInfo.style.display = "none";
+    iphoneInfo.style.display = "none";
+    model.style.color = "black";
+    line.style.backgroundColor = "black";
+    samsungInfo.style.color = "black";
+    h22.style.color = "black";
 }
